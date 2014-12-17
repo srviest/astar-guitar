1ef0dff934e3d837a07e5d57ed881e7d6f246e8b
gburlet <gregory.burlet@gmail.com>
2014-12-16 17:36:51 -0700

    Score read MusicXML
    as well as MEI

--- score/score.py
+++ score/score.py
@@ -20,8 +20,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
 
-from pymei import MeiDocument, MeiElement, XmlImport
 from scoreevent import Note, Chord
+
+pitch_names = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
 
 class Score(object):
 
@@ -32,21 +33,32 @@ class Score(object):
 
         # musical events occuring in the input score
         self.score_events = []
+        self.doc = None         # container for parsed music document
 
-    def parse_mei_str(self, mei_str):
+class MeiScore(Score):
+    '''
+    Initialize an MEI score
+    '''
+
+    def __init__(self):
+        super(MeiScore, self).__init__()
+
+        from pymei import MeiDocument, MeiElement, XmlImport
+
+    def parse_str(self, mei_str):
         '''
         Read an mei file from string and fill the score model
         '''
 
-        self.meidoc = XmlImport.documentFromText(mei_str)
+        self.doc = XmlImport.documentFromText(mei_str)
         self.parse_input()
 
-    def parse_mei_file(self, mei_path):
+    def parse_file(self, mei_path):
         '''
         Read an mei file and fill the score model
         '''
 
-        self.meidoc = XmlImport.documentFromFile(str(mei_path))
+        self.doc = XmlImport.documentFromFile(str(mei_path))
         self.parse_input()
 
     def parse_input(self):
@@ -54,8 +66,7 @@ class Score(object):
         Parse the score data into the internal data representation.
         '''
         
-        pitch_names = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-        measures = self.meidoc.getElementsByName('measure')
+        measures = self.doc.getElementsByName('measure')
         for m in measures:
             # only parse first staff (instrument), the instrument to convert to tablature
             staff = m.getChildrenByName('staff')[0]
@@ -93,3 +104,92 @@ class Score(object):
         id = note.getId()
 
         return Note(pname, oct, id)
+
+class MusicXMLScore(Score):
+    '''
+    Initialize a MusicXML score
+    '''
+
+    def __init__(self):
+        super(MusicXMLScore, self).__init__()
+
+        from lxml import etree
+
+    def parse_str(self, xml_str):
+        '''
+        Read a MusicXML file from string and fill the score model
+        '''
+
+        self.doc = etree.fromstring(xml_str)
+        self.parse_input()
+
+    def parse_file(self, xml_path):
+        '''
+        Read a MusicXML file and fill the score model
+        '''
+
+        self.doc = etree.parse(mxml_file)
+        self.parse_input()
+
+    def parse_input(self):
+        '''
+        Parse the score data into the internal data representation.
+        '''
+
+        notes = doc.findall("part/measure/note")
+        # postprocess to arrange notes into chords
+        notes_in_chord = []
+        for i, n in enumerate(notes):
+            note = self._handle_xml_note(n, i+1)
+            if len(notes_in_chord):
+                if n.find("chord"):
+                    # append to chord
+                    notes_in_chord.append(note)
+                else:
+                    if len(notes_in_chord) > 1:
+                        # chord is over
+                        chord = Chord(notes_in_chord)
+                        self.score_events.append(chord)
+                    else:
+                        # not a chord, it's a single note
+                        self.score_events.append(notes_in_chord[0])
+                    notes_in_chord = []
+
+            notes_in_chord.append(note)
+
+        # deal with note/chord at end of file
+        if len(notes_in_chord) == 1:
+            self.score_events.append(notes_in_chord[0])
+        elif len(notes_in_chord) > 1:
+            chord = Chord(notes_in_chord)
+            self.score_events.append(chord)
+
+    def _handle_xml_note(self, n, nid):
+        '''
+        Helper function that takes a MusicXML note element
+        and creates a Note object out of it.
+
+        PARAMETERS:
+        n (lxml element): note in xml format
+        nid (int): desired note id
+
+        RETURNS:
+        note (ScoreEvent.Note): note in internal note representation
+        '''
+
+        pname = n.findtext("pitch/step")
+        alter = n.findtext("pitch/alter")
+        if alter:
+            '''
+            Assume musescore is sane about applying accidentals and doesn't do
+            double sharps etc., or apply a flat to an F instead of just using an E for pitch
+            '''
+            alter = int(alter)
+            if int(alter) == 1:
+                pname += '#'
+            elif int(alter) == -1:
+                # convert flats to sharps
+                pname = pitch_names[pitch_names.index(pname)-1 % len(pitch_names)] + '#'
+        octave = int(n.findtext("pitch/octave"))
+
+        return Note(pname, octave, nid)
